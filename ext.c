f2fe9ebb6835066e43b240b66bda06d6c6a4806e
krzysckh <krzysckh@wp.pl>
2024-04-18 20:34:42 +0200

    moar

--- ext.c
+++ ext.c
@@ -5,10 +5,18 @@
 word
 prim_custom(int op, word a, word b, word c)
 {
+  int i;
+
   switch (op) {
   case 100:
     printf("Hello, World!\n");
     return ITRUE;
+  case 101:
+    for (i = 0; i < immval(a); ++i)
+      printf("wow %d!\n", i);
+    return ITRUE;
+  case 102:
+    return onum(2137, 0);
   default:
     return IFALSE;
   }
--- main.scm
+++ main.scm
@@ -3,6 +3,13 @@
 (define (print-hello)
   (sys-prim 100 #f #f #f))
 
+(define (print-wow n)
+  (sys-prim 101 n #f #f))
+
+(define (get-n)
+  (sys-prim 102 #f #f #f))
+
 (λ (_)
   (print-hello)
+  (print-wow (get-n))
   0)
