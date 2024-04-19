35d8a59ffd9728f99f8e2f26d24474020c1423a0
krzysckh <kpm@linux.pl>
2024-04-19 17:24:58 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,18 @@
 set -xe
 
 OWL_SOURCE_PATH=/home/kpm/nmojeprogramy/owl
+CFLAGS="-ggdb -DPRIM_CUSTOM -I/usr/local/include -I$OWL_SOURCE_PATH/c"
+LDFLAGS="-L/usr/local/lib"
+
+if [ `uname -s` = "OpenBSD" ]; then
+  LDFLAGS="$LDFLAGS -lglfw"
+fi
 
 ol -x c -o main.c main.scm
-clang -I$OWL_SOURCE_PATH/c -DPRIM_CUSTOM ext.c main.c -o main
+clang $CFLAGS $LDFLAGS ext.c main.c -o main
 
 ol -x c -o rl-main.c rl.scm
-clang -ggdb -I$OWL_SOURCE_PATH/c -DPRIM_CUSTOM rl.c rl-main.c -lraylib -lm -o rl-main
+clang $CFLAGS $LDFLAGS rl.c rl-main.c -lraylib -lm -o rl-main
 
 ol -x c -o rl-repl.c rl-repl.scm
-clang -ggdb -I$OWL_SOURCE_PATH/c -DPRIM_CUSTOM rl.c rl-repl.c -lraylib -lm -o rl-repl
+clang $CFLAGS $LDFLAGS rl.c rl-repl.c -lraylib -lm -o rl-repl
