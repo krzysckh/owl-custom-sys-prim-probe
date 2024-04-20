53f34de3cceb0dce4b60328d3752c83d86a82e62
krzysckh <krzysckh@wp.pl>
2024-04-20 19:47:42 +0200

    build for windows

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# -*- mode: sh; sh-basic-offset: 2 -*-
 
 set -xe
 
@@ -10,14 +11,29 @@ if [ `uname -s` = "OpenBSD" ]; then
   LDFLAGS="$LDFLAGS -lglfw"
 fi
 
-ol -x c -o main.c main.scm
-clang $CFLAGS $LDFLAGS ext.c main.c -o main
+build_unix() {
+  ol -x c -o main.c main.scm
+  clang $CFLAGS $LDFLAGS ext.c main.c -o main
 
-ol -x c -o rl-main.c rl.scm
-clang $CFLAGS $LDFLAGS rl.c rl-main.c -lraylib -lm -o rl-main
+  ol -x c -o rl-main.c rl.scm
+  clang $CFLAGS $LDFLAGS rl.c rl-main.c -lraylib -lm -o rl-main
 
-ol -x c -o rl-repl.c rl-repl.scm
-clang $CFLAGS $LDFLAGS rl.c rl-repl.c -lraylib -lm -o rl-repl
+  ol -x c -o rl-repl.c rl-repl.scm
+  clang $CFLAGS $LDFLAGS rl.c rl-repl.c -lraylib -lm -o rl-repl
 
-ol -x c -o sqlite-main.c sqlite.scm
-clang $CFLAGS $LDFLAGS sqlite-main.c sqlite.c -lsqlite3 -o sqlite-main
+  ol -x c -o sqlite-main.c sqlite.scm
+  clang $CFLAGS $LDFLAGS sqlite-main.c sqlite.c -lsqlite3 -o sqlite-main
+}
+
+build_win() {
+  [ -f ovm-win.c ] || wget https://raw.githubusercontent.com/krzysckh/owl-winrt/master/ovm.c -O ovm-win.c
+  [ -f libraylib.a ] || wget https://pub.krzysckh.org/libraylib.a
+
+  CFLAGS="-ggdb -DPRIM_CUSTOM -I$OWL_SOURCE_PATH/c -I/usr/local/include"
+  LDFLAGS="-L. -l:libraylib.a -lm -lwinmm -lgdi32 -lws2_32 -static"
+
+  ol -x c -o rl-main.c -C ovm-win.c rl.scm
+  x86_64-w64-mingw32-gcc $CFLAGS rl.c rl-main.c $LDFLAGS -o rl-main.exe
+}
+
+build_$1
